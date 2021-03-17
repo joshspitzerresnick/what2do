0e02000121fecea2707dee3cb2a73ace6e0e5fca
Nick Seel <nick.seel@gmail.com>
2021-03-16 20:05:15 -0500

    started outlining other activities

---
+++ what2do/app/src/main/java/com/example/what2do/SwipeActivity.java
@@ -0,0 +1,26 @@
+package com.example.what2do;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SwipeActivity extends Activity implements View.OnClickListener {
+    private Button doneButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        doneButton = findViewById(R.id.factors_done);
+        doneButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == doneButton.getId()) {
+            finish();
+        }
+    }
+}
