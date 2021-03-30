908a4f917ccbff5317ec889a2d4f2ec58ad8ec28
Jimmy Nguyen <jimmytnguyen19@gmail.com>
2021-03-29 20:15:29 -0500

    Confined factors sliders with their text fields

--- what2do/app/src/main/java/com/example/what2do/activities/FactorsActivity.java
+++ what2do/app/src/main/java/com/example/what2do/activities/FactorsActivity.java
@@ -3,9 +3,13 @@ package com.example.what2do.activities;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.SeekBar;
 
 import com.example.what2do.R;
 
@@ -14,6 +18,8 @@ public class FactorsActivity extends Activity implements View.OnClickListener {
     private AlertDialog.Builder dialogBuilder;
     private AlertDialog dialog;
     private Button continueButton, backButton;
+    private SeekBar priceSeek, distanceSeek;
+    private EditText priceField, distanceField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +28,76 @@ public class FactorsActivity extends Activity implements View.OnClickListener {
 
         readyButton = findViewById(R.id.factors_done);
         readyButton.setOnClickListener(this);
+
+        priceField = findViewById(R.id.editTextPriceField);
+        priceField.addTextChangedListener(new TextWatcher() {
+            public void afterTextChanged(Editable s) {}
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                try{
+                    if (s!=null) {
+                        float priceText = Float.parseFloat(s.toString());
+                        int value = (int) priceText;
+                        priceSeek.setProgress(value);
+                        /*priceField.setText("");
+                        priceField.append(s);*/
+                    }
+                }
+                catch (Exception exception) {
+                    priceSeek.setProgress(0);
+                    exception.printStackTrace();
+                }
+            }
+        });
+
+        distanceField = findViewById(R.id.editTextDistanceField);
+        distanceField.addTextChangedListener(new TextWatcher() {
+            public void afterTextChanged(Editable s) {}
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                try{
+                    if (s!=null) {
+                        float priceText = Float.parseFloat(s.toString());
+                        int value = (int) priceText;
+                        distanceSeek.setProgress(value);
+                        /*distanceField.setText("");
+                        distanceField.append(s);*/
+                    }
+                }
+                catch (Exception exception) {
+                    distanceSeek.setProgress(0);
+                    exception.printStackTrace();
+                }
+            }
+        });
+
+        priceSeek = findViewById(R.id.PriceSlider);
+        priceSeek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                String newText = "" + progress;
+                priceField.setText(newText);
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+
+        distanceSeek = findViewById(R.id.DistanceSlider);
+        distanceSeek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                String newText = "" + progress;
+                distanceField.setText(newText);
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
 
     }
 
